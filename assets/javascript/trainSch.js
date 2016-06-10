72421fc42533b319a78beba6d3e1edac49098edf
MikeH <mike123.henry@gmail.com>
2016-06-10 18:22:15 -0500

    finished egde case testing and fixes to modulas formulas

--- assets/javascript/trainSch.js
+++ assets/javascript/trainSch.js
@@ -1,76 +1,58 @@
-// Link to Firebase
-var clickData = new Firebase("https://mike123-trainsch.firebaseio.com/");
-var startTime;
-//https://mike123-trainsch.firebaseio.com/#-KJILw2nVxsIdwx0k8F5|0075578bd0f4386061993efbbd5edacd
+$(document).ready(function() {
+	// Link to Firebase
+	var clickData = new Firebase("https://mike123-trainsch.firebaseio.com/");
 
-//var clickData = new Firebase("https://flickering-heat-7798.firebaseio.com/");
+	//get data from the form if the submit button is pushed
+	$(document).on("click", '#adminInput', function() {
+		var trainNameEntered = $('#trainName').val().trim();
+		var destinationEntered = $('#destination').val().trim();
+		var frequencyEntered = $('#frequency').val().trim();
+		var firstTrainDpartEntered = $('#firstTrainDpartTime').val().trim();
+		//push the data to firebase
+		clickData.push({
+			"trainNameFB": trainNameEntered,
+			"destinationFB": destinationEntered,
+			"frequencyFB": frequencyEntered,
+			"firstTrainDpartFB": firstTrainDpartEntered,
+		})
 
-$(document).on("click", '#adminInput', function() {
-	var trainNameEntered = $('#trainName').val().trim();
-	var destinationEntered = $('#destination').val().trim();
-	var frequencyEntered = $('#frequency').val().trim();
-	var firstTrainDpartEntered = $('#firstTrainDpartTime').val().trim();
-	console.log("List of things: " + trainNameEntered+ " " + destinationEntered+ " "  + frequencyEntered+ " "  + firstTrainDpartEntered);
+	  //clear the entry form
+		trainNameEntered = $('#trainName').val("");
+		destinationEntered = $('#destination').val("");
+		frequencyEntered = $('#frequency').val("");
+		firstTrainDpartEntered = $('#firstTrainDpartTime').val("");
 
-startTime = new moment(firstTrainDpartEntered,'hh:mm');
-console.log('startTime = '+ startTime);
-//  var startTime = moment() + firstTrainDpartEntered;
-//    console.log(startTime);
+		return false;
+	});
 
-	clickData.push({
-		"trainNameFB": trainNameEntered,
-		"destinationFB": destinationEntered,
-		"frequencyFB": frequencyEntered,
-		"firstTrainDpartFB": firstTrainDpartEntered
-	})
-//"firstTrainDpartFB": startTime
+	clickData.on("child_added", function(childSnapshot){
+		var trainNameAppend = childSnapshot.val().trainNameFB;
+		var destinationAppend = childSnapshot.val().destinationFB;
+		var frequencyAppend = childSnapshot.val().frequencyFB;
+		var firstDepart = childSnapshot.val().firstTrainDpartFB;
+		var firstTrainMath = moment(firstDepart,"hh:mm");
+		var howManyMinutesElasped = moment().diff(firstTrainMath, "minutes");
+		var minToNextTrain;
+		//fix for the way % works for the first train to run
+		// 2 divided by 5 = 0 with a remainder of 2
+		//i.e. 2 % 5 = 2 but the train would actually be 3 min away so
+		if (howManyMinutesElasped - frequencyAppend <0 ) {
+			minToNextTrain = frequencyAppend - howManyMinutesElasped;
+		}
+		else{
+			// the time elasped is more that 1 train interval and modulas works as expected
+			//the modulas remainder is the minutes since the last train ran so subtract it from the frequency to get the time to the next train
+			minToNextTrain = frequencyAppend -(howManyMinutesElasped % frequencyAppend);
+		}
+		var nextTime1 =  moment().add(minToNextTrain, "minutes");
+		var nextTime = moment(nextTime1).format("LT");
+		var lastUpdate = moment().format("LT");
+		if (trainNameAppend === undefined || destinationAppend === undefined || frequencyAppend === undefined || nextTime === undefined || minToNextTrain === undefined ) {
+			//if all data is not present do nothing
+		}
+		else{
+			$('#trainSchTable').prepend("<tr class='tDataRow'><td class='tdata'>" + trainNameAppend + "</td><td class='tdata'>" + destinationAppend + "</td class='tdata'><td>" +  frequencyAppend + "</td><td class='tdata'>" +  nextTime + "</td><td class='tdata'>" +  minToNextTrain + "</td class='tdata'><td class='tdata'>" + lastUpdate + "</td class='tdata'></tr>");
+		}
+	}); //end on("child_added"
 
-  //clear the entry form
-	trainNameEntered = $('#trainName').val("");
-	destinationEntered = $('#destination').val("");
-	frequencyEntered = $('#frequency').val("");
-	firstTrainDpartEntered = $('#firstTrainDpartTime').val("");
-	return false;
-});
-
-clickData.on("child_added", function(childSnapshot){
-	var trainNameAppend = childSnapshot.val().trainNameFB;
-	var destinationAppend = childSnapshot.val().destinationFB;
-	var frequencyAppend = childSnapshot.val().frequencyFB;
-	var firstDepart = childSnapshot.val().firstTrainDpartFB;
-	var firstTrainMath = moment(firstDepart,"hh:mm");
-	var currentTime = moment();
-	var howManyMinutes = currentTime.diff(firstTrainMath, "minutes");
-	var minToNextTrain = firstTrainMath % frequencyAppend;
-	var nextTime =  currentTime.add(minToNextTrain, "minutes");
-	nextTime = moment(nextTime).format("hh:mm")
-
-	console.log("trainNameAppend = "+trainNameAppend);
-	console.log("destinationAppend = "+destinationAppend);
-	console.log("frequencyAppend = "+frequencyAppend);
-	console.log("firstDepart = "+firstDepart);
-	console.log("firstTrainMath = "+firstTrainMath);
-	console.log("currentTime = "+currentTime);
-	console.log("howManyMinutes = "+howManyMinutes);
-	console.log("minToNextTrain = "+minToNextTrain);
-	console.log("nextTime = "+nextTime);
-
-
-  //var currentMoment = moment();
-  //var startMoment = moment(new firstTrainDpartEntered);var currentTime = moment();
-	//var nextArrivalAppend = childSnapshot.val().nextArrivalFB;
-
-	/*var momentMonths = moment(new Date(appendDate));
-	var currentMoment = moment();
-	var howManyMonths = moment().diff(startTime, "minutes");
-	var howMuchPaid = howManyMonths * childSnapshot.val().rateGivenFB;
-	console.log(howManyMonths + " This is the months");
-	console.log(appendName);*/
-	$('#trainSchTable').prepend("<tr><td>" + trainNameAppend + "</td><td>" + destinationAppend + "</td><td>" +  frequencyAppend + "</td><td>" +  nextTime + "</td><td>" +  minToNextTrain + "</td></tr>");
-
-});
-
-
-///dateAdded: Firebase.ServerValue.TIMESTAMP
-//.on('child_added', function() {});
-
+})// end document).ready()
